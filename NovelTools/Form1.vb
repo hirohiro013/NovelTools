8a156cff2a1f9ecdc101814bed8ce3d37bb3a2ce
hirohiro013 <yuufuture.springfieldxd9@gmail.com>
2018-07-07 23:06:28 +0900

    Implement variable file-name

--- NovelTools/Form1.vb
+++ NovelTools/Form1.vb
@@ -1,15 +1,22 @@
 ﻿Imports System.IO
 Public Class Form1
+    Dim myform As FrmSelect
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         Dim csvRecords As New ArrayList()
         Dim lstDataColumns As New ArrayList()
 
-        lstDataColumns = ReadCsvFile("Category\アンダードッグ カテゴリ.csv")
+
+        myform = CType(Me.Owner, FrmSelect)
+        Dim filename As String
+        filename = myform.cmbFile.SelectedItem
+
+
+        lstDataColumns = ReadCsvFile("File\Category\" & filename & " カテゴリ.csv")
         If lstDataColumns Is Nothing Then
             Return
         End If
 
-        csvRecords = ReadCsvFile("File\アンダードッグ.csv") 'そのうちファイル名を変更できるようにする
+        csvRecords = ReadCsvFile("File\" & filename & ".csv") 'そのうちファイル名を変更できるようにする
         If csvRecords Is Nothing Then
             Return
         End If
@@ -97,43 +104,5 @@ Public Class Form1
         Application.Exit()
     End Sub
 
-    'Private Function Tabtab(ByRef targetData As String, ByRef keta As Integer, ByRef moji As String) As String
-    '    If targetData Is Nothing Then
-    '        targetData = ""
-    '    End If
 
-    '    Dim pad As Char
-
-    '    Dim value As String = String.Empty
-    '    'パディングする文字数を演算
-    '    '(文字数=桁-(対象文字列のバイト数-対象文字列の文字列数))
-    '    Dim PadLength As Integer = keta - (System.Text.Encoding.GetEncoding(65001).GetByteCount(targetData) - targetData.Length)
-    '    pad = Convert.ToChar(" ")
-
-    '    value = targetData.PadRight(PadLength, moji.ToCharArray()(0))
-
-    '    Return value
-
-    'End Function
-
-    '    Function padRight(ByVal st As String, ByVal len As Integer) As String
-    '        Dim len0 As Integer
-    '        len0 = LenB(st)
-    '        If len - len0 < 4 Then
-    '            Do Until len - len0 < 4
-    '                len = len + 1
-    '            Loop
-    '        Else
-    '            Return RTrim(st) & Space(len - len0)
-    '        End If
-    '    End Function
-
-    '    Private Shared Function LenB(ByVal str As String) As Integer
-    '        'Shift JISに変換したときに必要なバイト数を返す
-    '        Return System.Text.Encoding.GetEncoding(65001).GetByteCount(str)
-    '    End Function
-
-    '    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
-    '        Application.Exit()
-    '    End Sub
 End Class
---
+++ NovelTools/FrmSelect.Designer.vb
@@ -0,0 +1,98 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmSelect
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmbFile = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnView = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmbFile
+        '
+        Me.cmbFile.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmbFile.Font = New System.Drawing.Font("ＭＳ ゴシック", 12.0!)
+        Me.cmbFile.FormattingEnabled = True
+        Me.cmbFile.Location = New System.Drawing.Point(143, 6)
+        Me.cmbFile.Name = "cmbFile"
+        Me.cmbFile.Size = New System.Drawing.Size(642, 41)
+        Me.cmbFile.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("ＭＳ ゴシック", 12.0!)
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(95, 33)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "選択:"
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.Font = New System.Drawing.Font("ＭＳ ゴシック", 12.0!)
+        Me.btnExit.Location = New System.Drawing.Point(12, 167)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(199, 70)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "終了"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnView
+        '
+        Me.btnView.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnView.Font = New System.Drawing.Font("ＭＳ ゴシック", 12.0!)
+        Me.btnView.Location = New System.Drawing.Point(586, 167)
+        Me.btnView.Name = "btnView"
+        Me.btnView.Size = New System.Drawing.Size(199, 70)
+        Me.btnView.TabIndex = 3
+        Me.btnView.Text = "表示"
+        Me.btnView.UseVisualStyleBackColor = True
+        '
+        'FrmSelect
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(797, 249)
+        Me.Controls.Add(Me.btnView)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.cmbFile)
+        Me.Name = "FrmSelect"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "ファイル選択"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbFile As ComboBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnView As Button
+End Class
