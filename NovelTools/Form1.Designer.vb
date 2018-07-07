9539f3c14f92b45a4276693117c2fab99f46ec8c
hirohiro013 <yuufuture.springfieldxd9@gmail.com>
2018-07-07 21:59:30 +0900

    プロジェクト ファイルを追加します。

---
+++ NovelTools/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.lblSearch = New System.Windows.Forms.Label()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.btnNew = New System.Windows.Forms.Button()
+        Me.btnSubmit = New System.Windows.Forms.Button()
+        Me.lstData = New System.Windows.Forms.ListView()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtSearch.Font = New System.Drawing.Font("MS UI Gothic", 12.0!)
+        Me.txtSearch.Location = New System.Drawing.Point(121, 9)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(784, 39)
+        Me.txtSearch.TabIndex = 0
+        '
+        'lblSearch
+        '
+        Me.lblSearch.AutoSize = True
+        Me.lblSearch.Font = New System.Drawing.Font("MS UI Gothic", 12.0!)
+        Me.lblSearch.Location = New System.Drawing.Point(12, 15)
+        Me.lblSearch.Name = "lblSearch"
+        Me.lblSearch.Size = New System.Drawing.Size(86, 33)
+        Me.lblSearch.TabIndex = 1
+        Me.lblSearch.Text = "検索:"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSearch.AutoSize = True
+        Me.btnSearch.Font = New System.Drawing.Font("MS UI Gothic", 12.0!)
+        Me.btnSearch.Location = New System.Drawing.Point(911, 8)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(112, 43)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "表示"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'btnNew
+        '
+        Me.btnNew.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnNew.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnNew.Font = New System.Drawing.Font("MS UI Gothic", 12.0!)
+        Me.btnNew.Location = New System.Drawing.Point(18, 562)
+        Me.btnNew.Name = "btnNew"
+        Me.btnNew.Size = New System.Drawing.Size(182, 72)
+        Me.btnNew.TabIndex = 4
+        Me.btnNew.Text = "新規追加"
+        Me.btnNew.UseVisualStyleBackColor = True
+        '
+        'btnSubmit
+        '
+        Me.btnSubmit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.btnSubmit.Font = New System.Drawing.Font("MS UI Gothic", 12.0!)
+        Me.btnSubmit.Location = New System.Drawing.Point(240, 562)
+        Me.btnSubmit.Name = "btnSubmit"
+        Me.btnSubmit.Size = New System.Drawing.Size(182, 72)
+        Me.btnSubmit.TabIndex = 5
+        Me.btnSubmit.Text = "保存"
+        Me.btnSubmit.UseVisualStyleBackColor = True
+        '
+        'lstData
+        '
+        Me.lstData.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.lstData.Font = New System.Drawing.Font("ＭＳ ゴシック", 12.0!)
+        Me.lstData.Location = New System.Drawing.Point(18, 124)
+        Me.lstData.Name = "lstData"
+        Me.lstData.Size = New System.Drawing.Size(1005, 335)
+        Me.lstData.TabIndex = 6
+        Me.lstData.UseCompatibleStateImageBehavior = False
+        Me.lstData.View = System.Windows.Forms.View.Details
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.Font = New System.Drawing.Font("MS UI Gothic", 12.0!)
+        Me.btnExit.Location = New System.Drawing.Point(841, 562)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(182, 72)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.Text = "終了"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(13.0!, 24.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.ClientSize = New System.Drawing.Size(1035, 646)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSubmit)
+        Me.Controls.Add(Me.btnNew)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.lblSearch)
+        Me.Controls.Add(Me.txtSearch)
+        Me.Controls.Add(Me.lstData)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents lblSearch As Label
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents btnNew As Button
+    Friend WithEvents btnSubmit As Button
+    Friend WithEvents lstData As ListView
+    Friend WithEvents btnExit As Button
+End Class
---
+++ NovelTools/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim csvRecords As New ArrayList()
+        Dim lstDataColumns As New ArrayList()
+
+        lstDataColumns = ReadCsvFile("Category\アンダードッグ カテゴリ.csv")
+        If lstDataColumns Is Nothing Then
+            Return
+        End If
+
+        csvRecords = ReadCsvFile("File\アンダードッグ.csv") 'そのうちファイル名を変更できるようにする
+        If csvRecords Is Nothing Then
+            Return
+        End If
+
+        Dim csvFieldOut As New ArrayList
+        Dim lstDataColumnOut As New ArrayList
+
+        Dim i As Int32
+        Dim j As Int32
+        Dim k As Int32
+        For i = 0 To csvRecords.Count - 1
+
+            csvFieldOut = csvRecords(i)
+
+            lstData.Items.Add(csvFieldOut(0), i)
+
+            For j = 1 To csvFieldOut.Count - 1
+                lstData.Items(i).SubItems.Add(csvFieldOut(j))
+            Next
+        Next
+
+        For k = 0 To lstDataColumns.Count - 1
+            lstDataColumnOut = lstDataColumns(k)
+            If lstData.Items.Count > 0 Then
+                For i = 0 To lstDataColumnOut.Count - 1
+                    lstData.Columns.Add(lstDataColumnOut(i), 100, HorizontalAlignment.Left)
+                Next
+            End If
+        Next
+
+    End Sub
+
+
+    Private Function ReadCsvFile(name As String) As ArrayList
+
+        Dim strLine As String '1行
+        Dim strTemp() As String '戻り配列
+        Dim intCnt As Integer '配列添字
+        Dim strData As String = "" 'データ
+
+        Try
+            Dim objFile As New StreamReader(name, System.Text.Encoding.Default)
+            Dim csvRecords As New ArrayList()
+
+            '次の行へ
+            strLine = objFile.ReadLine()
+
+            While (strLine <> "")
+                '行単位データをカンマで分割し、配列に格納
+                strTemp = Split(strLine, ",")
+
+                'フィールドデータ用配列
+                Dim csvFields As New ArrayList
+
+                For intCnt = 0 To UBound(strTemp)
+                    '列データ strTemp(intCnt)を取得
+                    csvFields.Add(strTemp(intCnt))
+                Next
+
+                'レコードサイズを固定
+                csvFields.TrimToSize()
+                csvRecords.Add(csvFields)
+
+                '次の行へ
+                strLine = objFile.ReadLine()
+
+            End While
+            'レコードサイズを固定
+            csvRecords.TrimToSize()
+
+            'ファイルのクローズ
+            objFile.Close()
+
+            Return csvRecords
+
+        Catch ex As System.IO.FileNotFoundException
+            'ファイルが存在しないとき
+            MessageBox.Show(ex.Message)
+            Return Nothing
+        End Try
+
+    End Function
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Application.Exit()
+    End Sub
+
+    'Private Function Tabtab(ByRef targetData As String, ByRef keta As Integer, ByRef moji As String) As String
+    '    If targetData Is Nothing Then
+    '        targetData = ""
+    '    End If
+
+    '    Dim pad As Char
+
+    '    Dim value As String = String.Empty
+    '    'パディングする文字数を演算
+    '    '(文字数=桁-(対象文字列のバイト数-対象文字列の文字列数))
+    '    Dim PadLength As Integer = keta - (System.Text.Encoding.GetEncoding(65001).GetByteCount(targetData) - targetData.Length)
+    '    pad = Convert.ToChar(" ")
+
+    '    value = targetData.PadRight(PadLength, moji.ToCharArray()(0))
+
+    '    Return value
+
+    'End Function
+
+    '    Function padRight(ByVal st As String, ByVal len As Integer) As String
+    '        Dim len0 As Integer
+    '        len0 = LenB(st)
+    '        If len - len0 < 4 Then
+    '            Do Until len - len0 < 4
+    '                len = len + 1
+    '            Loop
+    '        Else
+    '            Return RTrim(st) & Space(len - len0)
+    '        End If
+    '    End Function
+
+    '    Private Shared Function LenB(ByVal str As String) As Integer
+    '        'Shift JISに変換したときに必要なバイト数を返す
+    '        Return System.Text.Encoding.GetEncoding(65001).GetByteCount(str)
+    '    End Function
+
+    '    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+    '        Application.Exit()
+    '    End Sub
+End Class
